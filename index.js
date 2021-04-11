2967c1c78cd0e7d592674fa0061e7ec780fc3bdf
Yuriy <yrambler2001@gmail.com>
2021-04-11 19:53:37 +0300

    eslint ✨

--- index.js
+++ index.js
@@ -1,34 +1,33 @@
 (async () => {
-  if (!window.auth_)
+  if (!window.auth_) {
     throw new Error(
       `please set auth variable auth_ to look like this:
       auth_='Bearer BQCA1LtI4pHpk5inoeazauv1m2K7wPOeLVswaA-BFEK_UvmUHOjNS4wqU2Ua7Cjd9Lj35k3Ao70AjZcumjqm5Sq26gTcMIbliAQrMhkcGgAabXuD1NNa0YP7n02qdDmdus4aD2PT9wml_05T93hsb3WX40azRR8VnN-NrMIgMArt1_sMa1rO1s0k-nVNOzpyl_n4PcQwGwIG1nd9cWmAVx6ZuXRhRlIWq_nYsINsga03NpDfrO8IUFqgX6lyuhaKk0za76U-hm986cEaR30qx-e5mFBRKaDwBA2Ojq-4Jaxl7A'
       you can copy this variable from spotify web player chrome dev tools requests tab
       `,
     );
+  }
   window.cache = window.cache || {};
   let cacheEnabled = true;
   const playlistName = 'All my songs';
   const cacheData = async (key, fnForData) => (cacheEnabled && window.cache[key]) || (window.cache[key] = await fnForData());
-  const fetchWithCache = async (...params) => cacheData(JSON.stringify(params), () => fetch(...params).then(r => r.json()));
-  const fetchWithCacheGet = async url =>
-    fetchWithCache(url, {
-      headers: {
-        accept: 'application/json',
-        authorization: window.auth_,
-      },
-      method: 'GET',
-    });
-  const fetchWithCachePost = async ({ url, data }) =>
-    fetchWithCache(url, {
-      headers: {
-        accept: 'application/json',
-        authorization: window.auth_,
-      },
-      body: JSON.stringify(data),
-      method: 'POST',
-    });
-  const getFormattedDate = date => {
+  const fetchWithCache = async (...params) => cacheData(JSON.stringify(params), () => fetch(...params).then((r) => r.json()));
+  const fetchWithCacheGet = async (url) => fetchWithCache(url, {
+    headers: {
+      accept: 'application/json',
+      authorization: window.auth_,
+    },
+    method: 'GET',
+  });
+  const fetchWithCachePost = async ({ url, data }) => fetchWithCache(url, {
+    headers: {
+      accept: 'application/json',
+      authorization: window.auth_,
+    },
+    body: JSON.stringify(data),
+    method: 'POST',
+  });
+  const getFormattedDate = (date) => {
     const year = date.getFullYear();
     const month = (1 + date.getMonth()).toString().padStart(2, '0');
     const day = date
@@ -38,7 +37,7 @@
 
     return `${day}-${month}-${year}`;
   };
-  const fetchWithPagination = async url => {
+  const fetchWithPagination = async (url) => {
     let next = url;
     const arr = [];
     while (next) {
@@ -71,10 +70,11 @@
     uriOfAllSongsPlaylist = createdPlaylist.id;
   }
 
-  for (const uris of chunk(songs.map(s => s.track.uri), 100))
+  for (const uris of chunk(songs.map((s) => s.track.uri), 100)) {
     await fetchWithCachePost({
       url: `https://api.spotify.com/v1/playlists/${uriOfAllSongsPlaylist}/tracks`,
       data: { uris, position: null },
     });
+  }
   console.log('ok');
 })();
